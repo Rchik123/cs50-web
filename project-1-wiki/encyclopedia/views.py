800b249b52ec512c079d4a44d5e59eef95f53a22
Revaz Chikovani <rchik20@freeuni.edu.ge>
2022-12-06 20:13:44 +0400

    add visiting entry pages by url (#1)

--- project-1-wiki/encyclopedia/views.py
+++ project-1-wiki/encyclopedia/views.py
@@ -8,3 +8,12 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    entry = util.get_entry(title)
+    if entry is None:
+        return render(request, "encyclopedia/entrynf.html")
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entry": entry,
+            "title": title
+        })
