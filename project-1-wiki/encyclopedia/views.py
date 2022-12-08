ecdc7463d8e433830ef0e5bba8a7a09b76ab681a
Rchik123 <rezichiko@gmail.com>
2022-12-08 18:20:58 +0400

    implemented search

--- project-1-wiki/encyclopedia/views.py
+++ project-1-wiki/encyclopedia/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
+from django import forms
 
 from . import util
 
-
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
     })
+
+def search(request):
+    print("asd")
+    if request.method == "GET":
+        input = request.GET.get("q", "")
+        entry = util.get_entry(input)
+        if entry is None:
+            titles = []
+            entries = util.list_entries()
+            for e in entries:
+                if e.__contains__(input):
+                    titles.append(e)
+            return render(request, "encyclopedia/searchnf.html", {
+                "titles": titles
+            })
+        else:
+            return render(request, "encyclopedia/entry.html", {
+                "entry": entry,
+                "title": input
+        })
+        
 
 def entry(request, title):
     entry = util.get_entry(title)
@@ -17,3 +38,4 @@ def entry(request, title):
             "entry": entry,
             "title": title
         })
+    
